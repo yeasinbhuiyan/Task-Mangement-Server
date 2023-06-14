5ce1bdb0898c7bf97ac8894ad01f6b7d70b2cd42
yeasinbhuiyan <yeasin382255656@gmail.com>
2023-06-15 02:36:08 +0600

    server initial work is complete and post , get or patch api complete

---
+++ index.js
@@ -0,0 +1,128 @@
+const express = require('express');
+const app = express()
+const cors = require('cors');
+const port = process.env.PORT || 5000
+require('dotenv').config()
+
+
+
+const corsOptions = {
+    origin: '*',
+    credentials: true,
+    optionSuccessStatus: 200,
+}
+app.use(cors(corsOptions))
+
+app.use(express.json())
+
+
+
+
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.9xgdj4e.mongodb.net/?retryWrites=true&w=majority`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
+});
+
+async function run() {
+    try {
+        // Connect the client to the server	(optional starting in v4.7)
+        await client.connect();
+        const database = client.db("MohitDB");
+        const taskCollection = database.collection("task");
+
+
+
+        app.post('/add-task', async (req, res) => {
+            const task = req.body
+            const result = await taskCollection.insertOne(task)
+            res.send(result)
+
+        })
+
+
+        app.get('/all-task', async (req, res) => {
+
+            const result = await taskCollection.find().toArray()
+            res.send(result)
+
+        })
+
+
+        app.patch('/task-update/:id', async (req, res) => {
+            const id = req.params.id
+            const filter = { _id: new ObjectId(id) }
+            const updatedDoc = {
+                $set: {
+
+                    status: 'complete'
+
+                }
+            }
+
+            const result = await taskCollection.updateOne(filter, updatedDoc)
+            res.send(result)
+
+        })
+
+
+        app.delete('/product/delete/:id', async (req, res) => {
+            const id = req.params.id
+            const query = { _id: new ObjectId(id) }
+            const result = await productsCollection.deleteOne(query)
+            res.send(result)
+
+        })
+
+
+
+        app.delete('/select-product/delete', async (req, res) => {
+
+            const selectedProducts = req.body
+            console.log(selectedProducts)
+            const query = { _id: { $in: selectedProducts.map(id => new ObjectId(id)) } }
+            // console.log(query)
+            const result = await productsCollection.deleteMany(query)
+            res.send(result)
+
+        })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        // Send a ping to confirm a successful connection
+        await client.db("admin").command({ ping: 1 });
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    } finally {
+        // Ensures that the client will close when you finish/error
+        // await client.close();
+    }
+}
+run().catch(console.dir);
+
+
+
+app.get('/', (req, res) => {
+    res.send('this server name is Blitz Camp')
+})
+
+app.listen(port, () => {
+    console.log(`this Server Running On ${port}`)
+})
+
